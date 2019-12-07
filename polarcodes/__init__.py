bb81708bc2b5bca706e6cb6578f3781bbd0155d2
Brendon McBain <brendon.mcbain9@gmail.com>
2019-12-07 18:23:32 +1100

    Update __init__.py

--- polarcodes/__init__.py
+++ polarcodes/__init__.py
@@ -3,20 +3,12 @@ Polar Codes in Python
 =============================================
 """
 
-from . import Math
-from . import Construct
-from . import Shorten
-from . import Encode
-from . import Decode
-from . import AWGN
-from . import Puncture
-
-__all__ = [
-    'Math',
-    'Construct',
-    'Shorten',
-    'Encode',
-    'Decode',
-    'AWGN',
-    'Puncture'
-]
+from polarcodes.Math import Math
+from polarcodes.Construct import Construct
+from polarcodes.Shorten import Shorten
+from polarcodes.Encode import Encode
+from polarcodes.Decode import Decode
+from polarcodes.AWGN import AWGN
+from polarcodes.Puncture import Puncture
+from polarcodes.PolarCode import PolarCode
+from polarcodes.GUI import GUI
